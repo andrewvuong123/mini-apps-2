79453244ba1e05e33d631930f1580d61cfce7d7b
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-11-04 16:49:45 -0800

    Implemented pagination

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 import Events from './events.jsx';
+import ReactPaginate from 'react-paginate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
-      query: ''
+      query: '',
+      offset: 0,
+      perPage: 10
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.loadEventsFromServer = this.loadEventsFromServer.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
 
   handleInputChange(event) {
@@ -18,21 +23,40 @@ class App extends React.Component {
       query: event.target.value
     })
     event.preventDefault();
-  }
+  };
 
   handleSubmit(event) {
+    // get page count from all queries returned
     axios.get(`/events?q=${this.state.query}`)
-      .then((response) => {
+      .then(res => {
         this.setState({
-          data: response.data
+          pageCount: Math.ceil(res.data.length / this.state.perPage)
+        }, this.loadEventsFromServer());
+      });
+    event.preventDefault();
+  };
+
+  loadEventsFromServer() {
+    // get limited results for each page
+    axios.get(`/events?q=${this.state.query}&_page=${this.state.offset}&_limit=10`)
+      .then(response => {
+        this.setState({
+          data: response.data,
         });
       })
       .catch(function (err) {
         console.log(err);
       });
+  };
 
-    event.preventDefault();
-  }
+  handlePageClick(data) {
+    let selected = data.selected;
+    let offset = Math.ceil(selected * this.state.perPage);
+
+    this.setState({ offset: offset }, () => {
+      this.loadEventsFromServer();
+    });
+  };
 
   render() {
     return (
@@ -44,6 +68,19 @@ class App extends React.Component {
           </label>
           <button type="submit">Search</button>
         </form>
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed = {2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
         <Events events={this.state.data} />
       </div>
     )
